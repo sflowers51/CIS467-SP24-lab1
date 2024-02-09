53b33b6a6d7186659a1e66678566a2e30576b7fe
Kenan Casey <kenancasey@gmail.com>
2024-02-09 10:38:19 -0600

    Add user stub and tags endpoint

--- app.js
+++ app.js
@@ -10,15 +10,23 @@ app.use( express.urlencoded( {extended: true} ))
 const pool = mysql.createPool( config.db) 
 
 app.listen(PORT, async () => {
-    console.log(`Listening on http://localhost:${PORT}`)
+    const host = process.env.HOSTNAME || "http://localhost"
+    console.log(`Listening on ${host}:${PORT}`)
+})
+
+app.use( (req, res, next)=> {
+    req.user = {id:4, name:"Kenan"}
+    next()
 })
 
 app.get('/', async (req, res)=> {
     try {
         const conn = await pool.getConnection();
-
+        console.log(req.user)
         const [users, ] = await conn.query("SELECT * FROM users")
-        console.log(users)
+
+        conn.release()
+        //console.log(users)
 
         res.json(users)
     }
@@ -26,7 +34,23 @@ app.get('/', async (req, res)=> {
         res.json( {message: "error"})
         console.error(err)
     }
+    
+})
 
+app.get('/tags', async (req, res)=> {
+    try {
+        const conn = await pool.getConnection();
+        console.log(req.user)
+        const [users, ] = await conn.query("SELECT * FROM tags")
 
+        conn.release()
+        //console.log(users)
+
+        res.json(users)
+    }
+    catch( err) {
+        res.json( {message: "error"})
+        console.error(err)
+    }
     
 })
