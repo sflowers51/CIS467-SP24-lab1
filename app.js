52056d72cc5ef22adba01502ad2d98875188a1a2
Kenan Casey <kenancasey@gmail.com>
2024-02-09 14:25:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const config = require('./config')
 app.use( express.json() )
 app.use( express.urlencoded( {extended: true} ))
 
-const pool = mysql.createPool( config.db) 
+const pool = mysql.createPool( config.db ) 
 
 app.listen(PORT, async () => {
     const host = process.env.HOSTNAME || "http://localhost"
@@ -41,16 +41,38 @@ app.get('/tags', async (req, res)=> {
     try {
         const conn = await pool.getConnection();
         console.log(req.user)
-        const [users, ] = await conn.query("SELECT * FROM tags")
+        const [tags, ] = await conn.query("SELECT * FROM tags")
 
         conn.release()
         //console.log(users)
 
-        res.json(users)
+        res.json(tags)
     }
     catch( err) {
         res.json( {message: "error"})
         console.error(err)
     }
-    
+})
+
+app.get('/tags/:id', async (req, res)=> {
+    try {
+        const conn = await pool.getConnection();
+        console.log(req.user)
+        const [tags, ] = await conn.query("SELECT * FROM tags WHERE tagID=" + req.params.id)
+
+        conn.release()
+        //console.log(users)
+
+        if( tags.length > 0) {
+            res.json(tags[0])
+        }
+        else {
+            res.status(404).json({message: "Resource not found"})
+        }
+
+    }
+    catch( err ) {
+        res.status(500).json( {message: "error"})
+        console.error(err)
+    }
 })
