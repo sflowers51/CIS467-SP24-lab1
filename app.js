23b607d7ecc960fedf93a177fbd28da304d6087a
Kenan Casey <kenancasey@gmail.com>
2024-02-12 15:32:05 -0600

    Add POST /tags endpoint

--- app.js
+++ app.js
@@ -1,78 +1,95 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
-const mysql = require('mysql2/promise')
-const config = require('./config')
+const express = require("express");
 
-app.use( express.json() )
-app.use( express.urlencoded( {extended: true} ))
+const app = express();
+const PORT = 3000;
+const mysql = require("mysql2/promise");
+const config = require("./config");
 
-const pool = mysql.createPool( config.db ) 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = mysql.createPool(config.db);
 
 app.listen(PORT, async () => {
-    const host = process.env.HOSTNAME || "http://localhost"
-    console.log(`Listening on ${host}:${PORT}`)
-})
+  const host = process.env.HOSTNAME || "http://localhost";
+  console.log(`Listening on ${host}:${PORT}`);
+});
 
-app.use( (req, res, next)=> {
-    req.user = {id:4, name:"Kenan"}
-    next()
-})
+app.use((req, res, next) => {
+  req.user = { id: 4, name: "Kenan" }
+  next()
+});
 
-app.get('/', async (req, res)=> {
-    try {
-        const conn = await pool.getConnection();
-        console.log(req.user)
-        const [users, ] = await conn.query("SELECT * FROM users")
+app.get("/", async (req, res) => {
+  try {
+    const conn = await pool.getConnection();
+    console.log(req.user);
+    const [users] = await conn.query("SELECT * FROM users");
 
-        conn.release()
-        //console.log(users)
+    conn.release();
+    //console.log(users)
 
-        res.json(users)
+    res.json(users);
+  } catch (err) {
+    res.json({ message: "error" });
+    console.error(err);
+  }
+});
+
+app.get("/tags", async (req, res) => {
+  try {
+    const conn = await pool.getConnection();
+    console.log(req.user);
+    const [tags] = await conn.query("SELECT * FROM tags");
+
+    conn.release();
+    //console.log(users)
+
+    res.json(tags);
+  } catch (err) {
+    res.json({ message: "error" });
+    console.error(err);
+  }
+});
+
+app.get("/tags/:id", async (req, res) => {
+  try {
+    const conn = await pool.getConnection();
+    console.log(req.user);
+    const [tags] = await conn.query(
+      "SELECT * FROM tags WHERE tagID=" + req.params.id
+    );
+
+    conn.release();
+    //console.log(users)
+
+    if (tags.length > 0) {
+      res.json(tags[0]);
+    } else {
+      res.status(404).json({ message: "Resource not found" });
     }
-    catch( err) {
-        res.json( {message: "error"})
-        console.error(err)
-    }
-    
-})
+  } catch (err) {
+    res.status(500).json({ message: "error" });
+    console.error(err);
+  }
+});
 
-app.get('/tags', async (req, res)=> {
-    try {
-        const conn = await pool.getConnection();
-        console.log(req.user)
-        const [tags, ] = await conn.query("SELECT * FROM tags")
-
-        conn.release()
-        //console.log(users)
-
-        res.json(tags)
-    }
-    catch( err) {
-        res.json( {message: "error"})
-        console.error(err)
-    }
-})
-
-app.get('/tags/:id', async (req, res)=> {
-    try {
-        const conn = await pool.getConnection();
-        console.log(req.user)
-        const [tags, ] = await conn.query("SELECT * FROM tags WHERE tagID=" + req.params.id)
-
-        conn.release()
-        //console.log(users)
-
-        if( tags.length > 0) {
-            res.json(tags[0])
-        }
-        else {
-            res.status(404).json({message: "Resource not found"})
-        }
-
-    }
-    catch( err ) {
-        res.status(500).json( {message: "error"})
-        console.error(err)
-    }
-})
+// Create a new user
+app.post("/tags", async (req, res) => {
+  const { tagDescription } = req.body;
+  try {
+    const connection = await pool.getConnection();
+    await connection.query("INSERT INTO tags (tagDescription) VALUES (?)", [
+      tagDescription,
+    ]);
+    const [newTag] = await connection.query(
+      "SELECT * FROM tags WHERE tagDescription=?",
+      [tagDescription]
+    );
+    connection.release();
+    res.status(201).json(newTag[0]);
+  } catch (err) {
+    console.error("Error creating user:", err);
+    res.status(500).send("Error creating user");
+  }
+});
